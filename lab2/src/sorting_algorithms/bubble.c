a59d5b043e88b74a489c718bdeb91a21e5f07bd9
Archit Yadav <archityadav97@gmail.com>
2021-03-09 21:35:21 +0100

    Efficient parallel Bubble sort

--- lab2/src/sorting_algorithms/bubble.c
+++ lab2/src/sorting_algorithms/bubble.c
@@ -39,24 +39,62 @@ void sequential_bubble_sort (uint64_t *T, const uint64_t size)
     return ;
 }
 
+int sequential_bubble_onepass(uint64_t *T, const uint64_t size)
+{
+    uint64_t i, temp;
+    uint64_t flag = 0;
+    // uint64_t sorted;
+
+    for(i = 0; i < size - 1; i++)
+    {
+        if (T[i] > T[i+1])
+        {
+            flag = 1;
+
+            temp = T[i+1];
+            T[i+1] = T[i];
+            T[i] = temp;
+
+            // sorted = 0;
+        }
+    }
+    if(flag == 1){ return 0; }  //=> sorted-->0 (this chunk was modified)
+    else return 1;              //=> sorted-->1 (this chunk was untouched)
+
+}
+
 void parallel_bubble_sort (uint64_t *T, const uint64_t size)
 {
     /* parallel implementation of bubble sort */
 
     uint64_t temp, sorted, i;
-    uint64_t ch_sz;
+    uint64_t ch_sz, ret_val;
     
     ch_sz = size / omp_get_max_threads();  
     do
     {
+        // print_array (T, size) ;
+
         sorted = 1;
-        #pragma omp parallel for schedule(static)
+        #pragma omp parallel for schedule(static), private(ret_val)
         for (i=0; i<size; i+=ch_sz)
         {
-            sequential_bubble_sort(T+i, ch_sz);
+            ret_val = sequential_bubble_onepass(T+i, ch_sz);
+            if(ret_val==0) { sorted = 0; }
+
+            // NOTE: We cannot simply directly assign the returned value to
+            // `sorted` variable, because there will be multiple returned
+            // values due to multiple threads.
+            // So we filter through all the returned values and only
+            // use it if it returns 0, which means the array was just modified,
+            // (by one of the threads) and hence the array may not be fully sorted
+            // which implies go through do-while again, in case the below for loop
+            // (border checking) does not make sorted = 0.
+
+            // TLDR: Even if one chunk is unsorted, `sorted` is assigned 0.
         }    
         #pragma omp parallel for schedule(static), private(temp)
-        for (i=ch_sz;i<size;i+=ch_sz)
+        for (i=ch_sz; i<size; i+=ch_sz)
         {
             if (T[i] < T[i-1])
             {
@@ -95,19 +133,19 @@ int main (int argc, char **argv)
     uint64_t *X = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
 
     printf(" --> Sorting an array of size %lu\n",N);
-#ifdef RINIT
+    #ifdef RINIT
     printf("--> The array is initialized randomly\n");
-#endif
+    #endif
     
 
-    for (exp = 0 ; exp < NBEXPERIMENTS; exp++){
-#ifdef RINIT
-        init_array_random (X, N);
-#else
-        init_array_sequence (X, N);
-#endif
+    for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+    {
+        #ifdef RINIT
+            init_array_random (X, N);
+        #else
+            init_array_sequence (X, N);
+        #endif
         
-      
         start = _rdtsc () ;
         
         sequential_bubble_sort (X, N) ;
@@ -116,21 +154,21 @@ int main (int argc, char **argv)
         experiments [exp] = end - start ;
 
         /* verifying that X is properly sorted */
-#ifdef RINIT
+        #ifdef RINIT
         if (! is_sorted (X, N))
         {
             fprintf(stderr, "ERROR: the sequential sorting of the array failed\n") ;
             print_array (X, N) ;
             exit (-1) ;
-	}
-#else
+	    }
+        #else
         if (! is_sorted_sequence (X, N))
         {
             fprintf(stderr, "ERROR: the sequential sorting of the array failed\n") ;
             print_array (X, N) ;
             exit (-1) ;
-	}
-#endif
+	    }
+        #endif
     }
 
     av = average_time() ;  
@@ -141,11 +179,11 @@ int main (int argc, char **argv)
   
     for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
     {
-#ifdef RINIT
-        init_array_random (X, N);
-#else
-        init_array_sequence (X, N);
-#endif
+        #ifdef RINIT
+            init_array_random (X, N);
+        #else
+            init_array_sequence (X, N);
+        #endif
         
         start = _rdtsc () ;
 
@@ -155,22 +193,21 @@ int main (int argc, char **argv)
         experiments [exp] = end - start ;
 
         /* verifying that X is properly sorted */
-#ifdef RINIT
-        if (! is_sorted (X, N))
-        {
-            fprintf(stderr, "ERROR: the parallel sorting of the array failed\n") ;
-            exit (-1) ;
-	}
-#else
-        if (! is_sorted_sequence (X, N))
-        {
-            fprintf(stderr, "ERROR: the parallel sorting of the array failed\n") ;
-            print_array (X, N) ;
-            exit (-1) ;
-	}
-#endif
-                
-        
+        #ifdef RINIT
+            if (! is_sorted (X, N))
+            {
+                fprintf(stderr, "ERROR: the parallel sorting of the array failed\n") ;
+                exit (-1) ;
+            }
+        #else
+            if (! is_sorted_sequence (X, N))
+            {
+                fprintf(stderr, "ERROR: the parallel sorting of the array failed\n") ;
+                print_array (X, N) ;
+                exit (-1) ;
+            }
+        #endif
+                  
     }
     
     av = average_time() ;  
@@ -184,11 +221,11 @@ int main (int argc, char **argv)
     uint64_t *Y = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
     uint64_t *Z = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
 
-#ifdef RINIT
-    init_array_random (Y, N);
-#else
-    init_array_sequence (Y, N);
-#endif
+    #ifdef RINIT
+        init_array_random (Y, N);
+    #else
+        init_array_sequence (Y, N);
+    #endif
 
     memcpy(Z, Y, N * sizeof(uint64_t));
 
