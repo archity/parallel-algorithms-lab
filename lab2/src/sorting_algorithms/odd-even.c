31b8d2f4986f02500c2f2b073291597855fba551
Archit Yadav <archityadav97@gmail.com>
2021-03-21 22:56:10 +0100

    Add Odd-Even Sort

--- lab2/src/sorting_algorithms/odd-even.c
+++ lab2/src/sorting_algorithms/odd-even.c
@@ -12,11 +12,38 @@
    odd-even sort -- sequential, parallel -- 
 */
 
+/* This is basically the same structure of Bubble sort, the main difference is 
+the step=2 in the For loops. In the 1st stage, we swap (if necessary) every 
+element in an odd position with the next one. In the 2nd one, we dp the same with
+every element in even pos.
+*/
 
+/*This implementation allows to do that in one For loop using startIndex to
+interleave between odds and evens.
+*/
 void sequential_oddeven_sort (uint64_t *T, const uint64_t size)
 {
     /* TODO: sequential implementation of odd-even sort */
+    uint64_t startIndex, i, temp, sorted;
 
+    
+    startIndex = 0;
+    do
+    {   
+        sorted = 1;
+        for(i = startIndex; i < size-1; i+=2)
+        {
+            if (T[i] > T[i+1])
+            {
+                temp = T[i+1];
+                T[i+1] = T[i];
+                T[i] = temp;
+
+                sorted = 0;
+            }
+        }
+        startIndex = 1 - startIndex;
+    } while (sorted == 0);
     return ;
 }
 
@@ -25,7 +52,41 @@ void parallel_oddeven_sort (uint64_t *T, const uint64_t size)
 {
 
     /* TODO: parallel implementation of odd-even sort */ 
+    uint64_t startIndex, i, temp, sorted, chunk_sz;
+    
+    chunk_sz = size / omp_get_max_threads();
+    // startIndex = 0;
+    do
+    {
+        sorted = 1;
+	/*We split the array in chunks and try to swap odds-possitioned elems inside each chunk with threads*/    
+        #pragma omp parallel for private(temp)
+        for(i = 0; i < size-1; i+=2)
+        {
+            if (T[i] > T[i+1])
+            {
+                temp = T[i+1];
+                T[i+1] = T[i];
+                T[i] = temp;
 
+                sorted = 0;
+            }
+        }
+	/*When all prevouis threads have finished, try to swap even-pos elems inside same chunk partitions*/
+        #pragma omp parallel for private(temp)
+        for(i = 1; i < size-1; i+=2)
+        {
+            if (T[i] > T[i+1])
+            {
+                temp = T[i+1];
+                T[i+1] = T[i];
+                T[i] = temp;
+
+                sorted = 0;
+            }
+        }
+        // startIndex = 1 - startIndex;
+    } while (sorted == 0);
     return ;
 }
 
@@ -48,7 +109,9 @@ int main (int argc, char **argv)
     /* the array to be sorted */
     uint64_t *X = (uint64_t *) malloc (N * sizeof(uint64_t)) ;
 
-    printf("--> Sorting an array of size %u\n",N);
+    printf("================================================\n");
+    printf(" Max number of threads: %d \n", omp_get_max_threads());    
+    printf(" --> Sorting an array of size %lu (2^%u)\n", N, atoi(argv[1]));
 #ifdef RINIT
     printf("--> The array is initialized randomly\n");
 #endif
@@ -89,7 +152,8 @@ int main (int argc, char **argv)
 
     av = average_time() ;  
 
-    printf ("\n odd-even serial \t\t\t %.2lf Mcycles\n\n", (double)av/1000000) ;
+    double serial_cycles = (double)av/1000000;
+    printf ("\n odd-even serial \t\t\t %.2lf Mcycles\n", (double)av/1000000) ;
 
   
     for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
@@ -118,6 +182,7 @@ int main (int argc, char **argv)
         if (! is_sorted_sequence (X, N))
         {
             fprintf(stderr, "ERROR: the parallel sorting of the array failed\n") ;
+            print_array (X, N) ;
             exit (-1) ;
 	}
 #endif
@@ -126,7 +191,14 @@ int main (int argc, char **argv)
     }
     
     av = average_time() ;  
+
+    double parallel_cycles = (double)av/1000000;
     printf ("\n odd-even parallel \t\t %.2lf Mcycles\n\n", (double)av/1000000) ;
+
+    FILE *f = fopen("speedups_odd.txt", "a+w");
+
+    printf(" Speedup: \t\t%f\n", serial_cycles/parallel_cycles);
+    fprintf(f, "%f\n", serial_cycles/parallel_cycles);
   
     /* print_array (X, N) ; */
 
@@ -155,4 +227,5 @@ int main (int argc, char **argv)
     free(Y);
     free(Z);
     
+    printf("================================================\n\n");
 }
